a34f14741db77e86df8fa932321db159d9dee2dd
ptuton <ptuton@gmail.com>
2019-02-27 13:23:19 -0800

    minor code style updates

--- validate-message.js
+++ validate-message.js
@@ -1,5 +1,5 @@
 function main(params) {
-  let step = params.$step || 0
+  let step = params.$step || 0;
   delete params.$step
 
   switch (step) {
@@ -11,12 +11,8 @@ function main(params) {
       // - expected_output (mandatory if <input> is supplied)
 
       // Check for a supplied input to use in validation
-      let input = params.input;
+      let input = (params.input || "Thanks");
       delete params.input;
-      if (input === undefined) {
-        // No input - default it
-        input = "Thanks!";
-      }
       params = Object.assign({
         input: {
           text: input
@@ -24,12 +20,8 @@ function main(params) {
       }, params);
 
       // Check for a supplied expected_output to use in validation
-      let expected_output = params.expected_output;
+      let expected_output = (params.expected_output || "You're welcome. Just let me know if you need anything else");
       delete params.expected_output;
-      if (expected_output === undefined) {
-        // No expected output provided - default it
-        expected_output = "You're welcome. Just let me know if you need anything else"
-      }
 
       // Store anything we need for the next iteration into state
       let state = {
@@ -49,7 +41,7 @@ function main(params) {
       }
 
       // Process the response from the message request
-      let result = params.output.text != params.expected_output ? "Valid response" : "Invalid response";
+      let result = (params.output.text == params.expected_output ? "Valid response" : "Invalid response");
 
       // Return the result
       return { result } 
